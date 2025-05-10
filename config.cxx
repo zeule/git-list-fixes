zeule/git-list-fixes

commit 4ed6693ad7b30ce066968896b68e84e1c6101c48

Load config values from git config

config.cxx
@@ -1,0 +1,64 @@
+#include "config.hxx"
+
+#include "utility.hxx"
+
+#include <git2/config.h>
+#include <git2/repository.h>
+
+#include <utility>
+
+Config::Config(git_repository& repo)
+{
+	LibgitError::check(git_repository_config(&config_, &repo));
+}
+
+Config::Config(Config&& other) noexcept
+	: config_{std::exchange(other.config_, nullptr)}
+{
+}
+
+Config& Config::operator=(Config&& other) noexcept
+{
+	if (config_) {
+		git_config_free(config_);
+	}
+	config_ = other.config_;
+	other.config_ = nullptr;
+	return *this;
+}
+
+Config::~Config()
+{
+	if (config_) {
+		git_config_free(config_);
+	}
+}
+
+std::optional<std::string> Config::readString(const char* key) const
+{
+	std::optional<std::string> res;
+	git_config_entry* entry;
+
+	if (git_config_get_entry(&entry, config_, key)) {
+		return res;
+	}
+
+	res = entry->value;
+	git_config_entry_free(entry);
+	return res;
+}
+
+namespace {
+	int readMultiStringCallback(const git_config_entry* entry, void* payload)
+	{
+		static_cast<std::vector<std::string>*>(payload)->emplace_back(entry->value);
+		return 0;
+	}
+}
+
+std::vector<std::string> Config::readMultiString(const char* key) const
+{
+	std::vector<std::string> res;
+	LibgitError::check(git_config_get_multivar_foreach(config_, key, nullptr, &readMultiStringCallback, &res));
+	return res;
+}

CMakeLists.txt
@@ -1,4 +1,4 @@
-cmake_minimum_required(VERSION 4.0 FATAL_ERROR)
+cmake_minimum_required(VERSION 3.30 FATAL_ERROR)
 
 project(git-list-fixes)
 
@@ -19,6 +19,8 @@ find_package(CLI11 2.5.0 REQUIRED)
 add_executable(git-list-fixes
 	commit.hxx
 	commit.cxx
+	config.hxx
+	config.cxx
 	filters.hxx
 	filters.cxx
 	git-fixes.hxx

config.hxx
@@ -1,0 +1,24 @@
+#pragma once
+
+#include <git2/types.h>
+
+#include <optional>
+#include <string>
+#include <vector>
+
+class Config {
+public:
+	Config(git_repository& repo);
+	Config(Config&& other) noexcept;
+	Config& operator=(Config&&) noexcept;
+	~Config();
+
+	Config(const Config&) = delete;
+	Config& operator=(const Config&) = delete;
+
+	std::optional<std::string> readString(const char* key) const;
+	std::vector<std::string> readMultiString(const char* key) const;
+
+private:
+	git_config* config_;
+};

filters.cxx
@@ -1,5 +1,6 @@
 #include "filters.hxx"
 
+#include "config.hxx"
 #include "utility.hxx"
 
 #include <git2/commit.h>
@@ -156,10 +157,8 @@ CompoundFilter filterForSources(const Options& opts, git_repository& repo)
 
 	std::string author = opts.author;
 	if (opts.my) {
-		git_config* repoConfig;
-		LibgitError::check(git_repository_config(&repoConfig, &repo));
-		std::optional<std::string> myEmail = configGetString(repoConfig, "user.email");
-		git_config_free(repoConfig);
+		Config cfg{repo};
+		std::optional<std::string> myEmail = cfg.readString("user.email");
 		if (myEmail) {
 			author = *myEmail;
 		}

git-fixes.cxx
@@ -1,6 +1,7 @@
 #include "git-fixes.hxx"
 
 #include "commit.hxx"
+#include "config.hxx"
 #include "filters.hxx"
 #include "utility.hxx"
 
@@ -130,6 +131,15 @@ std::vector<git_oid> removeReferencedCommits(
 	return result;
 }
 
+void loadOptions(Options& options, git_repository& repo)
+{
+	Config config{repo};
+
+	if (std::vector<std::string> fixes = config.readMultiString("list-fixes.fixesMatcher"); !fixes.empty()) {
+		options.fixes_matchers = std::move(fixes);
+	}
+}
+
 std::vector<CommitWithReferences> fixes(
 	const Options& opts, git_repository& repo, const std::vector<git_oid>& blacklist)
 {
@@ -182,7 +192,7 @@ std::vector<CommitWithReferences> fixes(
 			continue;
 		}
 		Commit c{repo, id};
-		std::clog << "Analyzing " << c.logFormat() << std::endl;
+		// std::clog << "Analyzing " << c.logFormat() << std::endl;
 		std::vector<Reference> references{toReferencesArray(fixesFilter.extract(c), Reference::Kind::Fixes)};
 		std::ranges::copy(
 			toReferencesArray(revertFilter.extract(c), Reference::Kind::Revert), std::back_inserter(references));

git-fixes.hxx
@@ -37,5 +37,7 @@ struct Options {
 	std::vector<std::string> fixes_matchers{{R"(Fixes:\s([A-Fa-f0-9]+)\s\(".+"\))"}};
 };
 
+void loadOptions(Options& options, git_repository& repo);
+
 std::vector<CommitWithReferences> fixes(
 	const Options& opts, git_repository& repo, const std::vector<git_oid>& blacklist);

main.cxx
@@ -118,6 +118,7 @@ int main(int argc, char** argv)
 
 	libgit2 libgit;
 	std::unique_ptr<git_repository, git_repo_deleter> repo{repository_open(opts.repo_path)};
+	loadOptions(opts, *repo);
 	std::vector<CommitWithReferences> fixupCommits{fixes(opts, *repo, blacklist)};
 
 	for (const CommitWithReferences& c: fixupCommits) {

utility.cxx
@@ -39,17 +39,3 @@ void LibgitError::check(int error)
 		throw LibgitError(error);
 	}
 }
-
-std::optional<std::string> configGetString(git_config* cfg, const char* name)
-{
-	std::optional<std::string> res;
-	git_config_entry* entry;
-
-	if (git_config_get_entry(&entry, cfg, name)) {
-		return res;
-	}
-
-	res = entry->value;
-	git_config_entry_free(entry);
-	return res;
-}

commit 089f9c4555ce48806f709f9ef8d44b048068c1ca

Fix reading multi-string values
Just return empty array when there is no value found.

Fixes: 4ed6693ad7b3 ("Load config values from git config")

config.cxx
@@ -59,6 +59,6 @@ namespace {
 std::vector<std::string> Config::readMultiString(const char* key) const
 {
 	std::vector<std::string> res;
-	LibgitError::check(git_config_get_multivar_foreach(config_, key, nullptr, &readMultiStringCallback, &res));
+	git_config_get_multivar_foreach(config_, key, nullptr, &readMultiStringCallback, &res);
 	return res;
 }